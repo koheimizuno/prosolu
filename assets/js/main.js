82bc89242eacfb72f5dfc2dc072b05cc5ba657cd
koheimizuno <skygentleman626@gmail.com>
2024-12-03 09:38:56 +0900

    fix link issue of hamburger

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,11 @@ $(document).ready(function () {
     $(".nav-links").toggleClass("active");
   });
 });
+
+function scrollToSection(sectionId) {
+  $("html, body").animate(
+    { scrollTop: $(`#${sectionId}`).offset().top - 95 },
+    1000
+  );
+  $(".nav-links").removeClass("active");
+}
